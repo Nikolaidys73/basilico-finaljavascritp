aa23df2a11ce63182682014aeb640c5ac4876485
nikolaidys73 <nicolasbasilico@gmail.com>
2023-01-21 10:32:29 -0300

    first commit

---
+++ js/cot.js
@@ -0,0 +1,11 @@
+$.ajax({
+    url: 'https://ws.geeklab.com.ar/dolar/get-dolar-json.php',
+    dataType: 'json',
+    success: function(data) {
+          $('#blue').html('$' + data.blue + ' USD');
+      $('#oficial').html('$' + data.libre + ' USD');
+      }
+  });
+  
+
+  /* https://codepen.io/ianaya89/pen/xbvrBx */
